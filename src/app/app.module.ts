0c35ceabb7f15dae75e1fe2bad7cde01e0dfec7f
davescripts <dave@davescripts.com>
2019-06-24 22:02:37 -0700

    Create the TestFormComponent.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { TestFormComponent } from './test-form/test-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestFormComponent
   ],
   imports: [
     BrowserModule,
