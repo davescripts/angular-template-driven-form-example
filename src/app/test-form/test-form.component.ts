2eaa889eb7ac5a0e78fb736ea9769fccffd15813
davescripts <dave@davescripts.com>
2019-06-24 22:11:53 -0700

    Update the TestFormComponent.

--- src/app/test-form/test-form.component.ts
+++ src/app/test-form/test-form.component.ts
@@ -1,11 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+import { TestData } from '../test-data';
+
 @Component({
   selector: 'app-test-form',
   templateUrl: './test-form.component.html',
   styleUrls: ['./test-form.component.css']
 })
 export class TestFormComponent implements OnInit {
+
+  testData = new TestData(
+    1,                    // Id
+    'My Name',            // Name
+    'My Comments',        // Comments
+    'Blue',               // Favorite Color
+    'Spring',             // Favorite Season
+    [false, true, true, false], // Hobbies
+  );
+
+  colors  = ['Red', 'Blue', 'Yellow', 'Purple', 'Orange', 'Green'];
+  seasons = ['Spring', 'Summer', 'Autumm', 'Winter'];
+  hobbies = ['Reading', 'Writing', 'Painting', 'Walking'];
 
   constructor() { }
 
